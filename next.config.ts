f5da07547bf397393ad0f398e73ebb34107ac587
Trucking KDY <kdytrucking@gmail.com>
2025-12-04 03:19:10 +0000

    Try fixing this error: `Console Error: Google Maps JavaScript API error:

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import 'dotenv/config';
 import type {NextConfig} from 'next';
 
-// Read the GOOGLE_MAPS_API_KEY from .env file
-process.env.GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY ?? '';
-
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
